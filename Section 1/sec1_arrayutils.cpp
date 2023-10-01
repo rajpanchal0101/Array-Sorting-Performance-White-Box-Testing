de8e484cfb00c5f68195e7a8124feb2fe1674e23
Raj Panchal <roger71011112000@gmail.com>
2023-10-01 07:51:00 +0530

    Internal documentation added to implementationfile

--- Section 1/sec1_arrayutils.cpp
+++ Section 1/sec1_arrayutils.cpp
@@ -5,15 +5,16 @@ vector<int> gen_rand_arr(int size)
     vector<int> arr(size);
     for (int i = 0; i < size; ++i)
     {
-        arr[i] = rand(); // Use rand() to generate random integers
+        arr[i] = rand(); //! generates random integers
     }
     return arr;
 }
 
+
 double meas_sortime(vector<int>& arr)
 {
-    auto start_time = chrono::high_resolution_clock::now();
-    sort(arr.begin(), arr.end());
-    auto end_time = chrono::high_resolution_clock::now();
-    return chrono::duration_cast<chrono::milliseconds>(end_time - start_time).count() / 1000.0; // Convert to seconds
+    auto start_time = chrono::high_resolution_clock::now(); //! Gets the current time before sorting
+    sort(arr.begin(), arr.end()); //! Sorts the array
+    auto end_time = chrono::high_resolution_clock::now();  //! Gets the current time after sorting
+    return chrono::duration_cast<chrono::milliseconds>(end_time - start_time).count() / 1000.0; //! Converts result to seconds
 }
