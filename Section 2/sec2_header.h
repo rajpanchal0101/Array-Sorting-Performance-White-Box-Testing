d62ba14b5af1e761a9882bd9125e8cdf94bc2402
Raj Panchal <roger71011112000@gmail.com>
2023-10-02 03:47:44 +0530

    Section2 header file

---
+++ Section 2/sec2_header.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include <cmath> // For reference calculations
+
+using namespace std;
+
+#include "S2.cpp"
+
+string testWithFloatingPointError(int xInput, int expectedOutput, const string& functionName);
